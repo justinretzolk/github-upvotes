0f7adeb377da2ac28d4171202b962516408c53cb
Justin Retzolk <44710313+justinretzolk@users.noreply.github.com>
2023-11-06 20:30:29 -0600

    improve concurrency

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/shurcooL/githubv4"
 )
 
 // init validates that the required variables are set before running
-func init() {
+func validateEnv() error {
 	requiredVars := []string{"GITHUB_TOKEN", "GITHUB_ORGANIZATION", "GITHUB_OUTPUT", "PROJECT_NUMBER", "UPVOTE_FIELD_NAME"}
 	var missing []string
 
@@ -18,47 +21,61 @@ func init() {
 	}
 
 	if len(missing) > 0 {
-		slog.Error(fmt.Sprintf("Missing required environment variables: %v", missing))
-		os.Exit(1)
+		return fmt.Errorf("missing required environment variables: %v", missing)
 	}
+
+	return nil
 }
 
 func main() {
+	defer timer(time.Now())()
+
+	if err := validateEnv(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 
 	c, err := NewCalculator()
 	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
+	defer output(&c.cursor)()
 
-	cursor, err := c.CalculateUpvotes()
+	err = c.CalculateUpvotes()
 	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
+}
 
-	if cursor != nil {
-		if err = output(*cursor); err != nil {
-			slog.Error(err.Error())
-			os.Exit(1)
-		}
+func timer(t time.Time) func() {
+	return func() {
+		slog.Info(fmt.Sprintf("elapsed time: %v", time.Since(t)))
 	}
 }
 
 // output writes the cursor to the file at $GITHUB_OUTPUT
-func output(cursor string) error {
-	path := os.Getenv("GITHUB_OUTPUT")
-	output := fmt.Sprintf("cursor=%s\n", cursor)
+func output(cursor *githubv4.String) func() {
+	return func() {
+		var c string
+		if cursor != nil {
+			c = (string)(*cursor)
+		}
 
-	outputFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		return err
+		path := os.Getenv("GITHUB_OUTPUT")
+		output := fmt.Sprintf("cursor=%s\n", c)
+
+		outputFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			slog.Error(err.Error())
+			os.Exit(1)
+		}
+
+		defer outputFile.Close()
+		if _, err := outputFile.WriteString(output); err != nil {
+			slog.Error(err.Error())
+			os.Exit(1)
+		}
 	}
-
-	defer outputFile.Close()
-	if _, err := outputFile.WriteString(output); err != nil {
-		return err
-	}
-
-	return nil
 }
